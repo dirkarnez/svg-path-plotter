19dc444e7ec43d0544490935bbcec14b041c0fcb
Dirk Arnez <smalldirkalex@gmail.com>
2023-10-13 21:35:16 +0800

    - update index.js

--- index.js
+++ index.js
@@ -7,3 +7,25 @@ var path = draw.path(`M0 0 ${Array(100).fill("").map((_, i) => {
 }).join(" ")}`)
 path.fill('none').move(20, 20)
 path.stroke({ color: '#f06', width: 4, linecap: 'round', linejoin: 'round' })
+
+
+function generateCirclePathElement(cx, cy, radius) {
+  var path = document.createElementNS("http://www.w3.org/2000/svg", "path");
+
+  var d = "M" + (cx - radius) + "," + cy;
+
+  // Approximation of a circle using cubic Bézier curves
+  d += "C" + (cx - radius) + "," + (cy + radius / 3) + " " +
+       (cx + radius) + "," + (cy + radius / 3) + " " +
+       (cx + radius) + "," + cy;
+
+  d += "C" + (cx + radius) + "," + (cy - radius / 3) + " " +
+       (cx - radius) + "," + (cy - radius / 3) + " " +
+       (cx - radius) + "," + cy;
+
+  path.setAttribute("d", d);
+  path.setAttribute("stroke", "black");
+  path.setAttribute("fill", "none");
+
+  return path;
+}
